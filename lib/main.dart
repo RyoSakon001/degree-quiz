b92836c6ca06f01dde1420a221e210dcf4c40eee
sakon-ziwave <ryo.development0320@gmail.com>
2023-01-08 16:39:10 +0900

    urlを設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
+import 'package:degree_quiz/home_page.dart';
 import 'package:degree_quiz/tutorial_page.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      routes: {
+        '/': (context) => const HomePage(),
+        '/tutorial': (context) => TutorialPage(),
+      },
+      initialRoute: "/",
       title: 'Flutter Overboard Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: TutorialPage(title: 'Flutter Overboard Home Page'),
     );
   }
 }
