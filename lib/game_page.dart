782929e40f7db227efc4f2dea9cf4dcc10065b4f
sakon-ziwave <ryo.development0320@gmail.com>
2023-01-08 19:30:17 +0900

    ゲームページのひな形を実装（カウンターアプリ）

---
+++ lib/game_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class GamePage extends StatefulWidget {
+  const GamePage();
+
+  @override
+  _GamePageState createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('戻る'),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('条件'),
+                ),
+              ],
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'You have pushed the button this many times:',
+                  ),
+                  Text(
+                    '$_counter',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    ));
+  }
+}
