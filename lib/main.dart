9c9cd937ea8f56cc4bb02ec0ebf0e1a0ab2d9f88
sakon-ziwave <ryo.development0320@gmail.com>
2023-01-08 13:06:23 +0900

    チュートリアルページを別ファイルに切り出し＋名称変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:degree_quiz/home.dart';
+import 'package:degree_quiz/tutorial_page.dart';
 import 'package:flutter/material.dart';
-
-import 'package:flutter_overboard/flutter_overboard.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,81 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Overboard Home Page'),
+      home: TutorialPage(title: 'Flutter Overboard Home Page'),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  final GlobalKey<ScaffoldState> _globalKey = new GlobalKey<ScaffoldState>();
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      key: _globalKey,
-      body: OverBoard(
-        allowScroll: true,
-        pages: pages,
-        showBullets: true,
-        inactiveBulletColor: Colors.blue,
-        skipCallback: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: ((context) => const HomePage()),
-            ),
-          );
-        },
-        finishCallback: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: ((context) => const HomePage()),
-            ),
-          );
-        },
-      ),
-    );
-  }
-
-  final pages = [
-    PageModel(
-        color: const Color(0xFF0097A7),
-        imageAssetPath: 'assets/images/image01.jpeg',
-        title: 'Screen 1',
-        body: 'Share your ideas with the team',
-        doAnimateImage: true),
-    PageModel(
-        color: const Color(0xFF536DFE),
-        imageAssetPath: 'assets/images/image02.jpeg',
-        title: 'Screen 2',
-        body: 'See the increase in productivity & output',
-        doAnimateImage: true),
-    PageModel(
-        color: const Color(0xFF9B90BC),
-        imageAssetPath: 'assets/images/image03.jpeg',
-        title: 'Screen 3',
-        body: 'Connect with the people from different places',
-        doAnimateImage: true),
-    PageModel.withChild(
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 25.0),
-          child: Image.asset('assets/images/image04.jpeg',
-              width: 300.0, height: 300.0),
-        ),
-        color: const Color(0xFF5886d6),
-        doAnimateChild: true)
-  ];
 }
