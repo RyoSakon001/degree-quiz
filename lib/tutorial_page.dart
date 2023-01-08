27c2546ceaa9d3d1c36beb72b05c394a9877f454
sakon-ziwave <ryo.development0320@gmail.com>
2023-01-08 19:09:00 +0900

    チュートリアルの説明を追加・画像変更

--- lib/tutorial_page.dart
+++ lib/tutorial_page.dart
@@ -30,52 +30,49 @@ class _TutorialPageState extends State<TutorialPage>
         inactiveBulletColor: Colors.blue,
         skipCallback: () {
           _setTutorialDone();
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: ((context) => const HomePage()),
-            ),
-          );
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: ((context) => const HomePage()),
+          ));
         },
         finishCallback: () {
           _setTutorialDone();
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: ((context) => const HomePage()),
-            ),
-          );
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: ((context) => const HomePage()),
+          ));
         },
       ),
     );
   }
 
   final pages = [
-    PageModel(
-        color: const Color(0xFF0097A7),
-        imageAssetPath: 'assets/images/image01.jpeg',
-        title: 'Screen 1',
-        body: 'Share your ideas with the team',
-        doAnimateImage: true),
-    PageModel(
-        color: const Color(0xFF536DFE),
-        imageAssetPath: 'assets/images/image02.jpeg',
-        title: 'Screen 2',
-        body: 'See the increase in productivity & output',
-        doAnimateImage: true),
-    PageModel(
-        color: const Color(0xFF9B90BC),
-        imageAssetPath: 'assets/images/image03.jpeg',
-        title: 'Screen 3',
-        body: 'Connect with the people from different places',
-        doAnimateImage: true),
-    PageModel.withChild(
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 25.0),
-          child: Image.asset('assets/images/image04.jpeg',
-              width: 300.0, height: 300.0),
-        ),
-        color: const Color(0xFF5886d6),
-        doAnimateChild: true)
+    _SimplePageModel(
+      title: 'このアプリの目的',
+      body:
+          '高校化学では、「物質量(mol)」について理解し、計算をスムーズにできるようにすることが、必須です。このアプリでは、物質量の計算をスキマ時間に行い、基礎力を高めることを目的としています。',
+      backgroundColor: Colors.lime,
+    ),
+    _SimplePageModel(
+      title: "Let's Start!",
+      body: '１回につき１０問を出題します。全問正解できるように頑張ってください！',
+      backgroundColor: Colors.amber,
+    ),
   ];
+}
+
+class _SimplePageModel extends PageModel {
+  _SimplePageModel({
+    required String title,
+    required String body,
+    required Color backgroundColor,
+  }) : super(
+          color: backgroundColor,
+          imageAssetPath: 'assets/images/frasco.png',
+          title: title,
+          titleColor: Colors.black,
+          body: body,
+          bodyColor: Colors.black,
+          doAnimateImage: true,
+        );
 }
 
 void _setTutorialDone() async {
